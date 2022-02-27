b61d2472fa56d5d308b158cbbf1830be6eb6226a
cpurificacao <carlos.purificacao@outlook.com>
2022-02-27 12:33:27 -0300

    feat: Timer service
    This service aims to implement the timer countdown behavior.

--- src/@types/timer.d.ts
+++ src/@types/timer.d.ts
@@ -1,8 +1,12 @@
-export interface TargetTime {
+export interface Time {
   hours: number;
   minutes: number;
   seconds: number;
 }
+
+export interface TargetTime extends Time {}
+
+export interface TimerUpdatePayload extends Time {}
 
 export default interface TimerProps {
   targetTime: TargetTime;
